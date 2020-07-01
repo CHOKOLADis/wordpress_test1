7a8d9a6e5c55acc0dc2eded544891586006b9142
CHOKOLADis <igor2011__igor@mail.ru>
2020-07-01 08:38:58 +0500

    5.0

--- js/script.js
+++ js/script.js
@@ -5,3 +5,25 @@ $(function(){
                 return false;
         });
 });
+
+$(window).scroll(function() {
+        $('.sc_title .letter').each(function(){
+                var imagePos = $(this).offset().top;
+
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow+800) {
+                $(this).addClass("fadeIn");
+                }
+        });
+});
+$(window).scroll(function() {
+        $('.sc_title h2').each(function(){
+                var imagePos = $(this).offset().top;
+
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow+800) {
+                $(this).addClass("fadeInUp");
+                }
+        });
+});
+    
